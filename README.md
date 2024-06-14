a93972699611bd3c36ec0c395ea2fa6633521e16
AlekUp24 <alekp24@gmail.com>
2024-06-14 11:36:14 +0200

    db set up ahead

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ Take a YouTube video and make it a blog post.
 
 CSS : Tailwind
 BACKEND: Django
+DB : PostgreSQL (Online - Quovery)
 
 
 01:02
--- backend/ai_blog_app/blog_generator/views.py
+++ backend/ai_blog_app/blog_generator/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def signup(request):
+    return render(request, 'signup.html')
+
+def logout(request):
+    return render(request, 'login.html')
